25adaa6414105deeb48020eadabf972de12ebbab
ReyesMak <reyesmakhh16@gmail.com>
2023-12-02 17:17:36 +0800

    Bisection Search

--- BisectionSearch.py
+++ BisectionSearch.py
@@ -1,22 +1,17 @@
 #Finding Root x
 
-import timeit
-start = timeit.default_timer()
+import timeit 
 
-x = 1.21
-epsilon = 0.001
-step = epsilon**2
-numGuesses = 0
-ans = 0.0
-
-while abs(ans**2 - x) >= epsilon and ans <= x:
-    ans += step
-    numGuesses += 1
-print('numGuesses =', numGuesses)
-
-if abs(ans**2 - x) >= epsilon:
-    print('Failed on square root of', x)
-else:
-    print(ans, 'is close to square root of', x)
-
-print(f'Total configuration execution time: {(timeit.default_timer() - start):.4f}s.', flush=True)
+def square_root_bSearch(x, epsilon):
+    if x < 0:
+        return None
+    low = min(-1.0, x)
+    high = max(1.0, x)
+    ans = (high + low)/2.0
+    while abs(ans**2 - x) >= epsilon:
+        if ans**2 < x:
+            low = ans
+        else:
+            high = ans
+        ans = (high + low)/2.0
+    return ans
